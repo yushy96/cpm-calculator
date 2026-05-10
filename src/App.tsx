8acd643bd8acd2e8eb143b2189de36a9502ae273
yushy96 <jshinkarenko1996@gmail.com>
2026-05-10 14:05:29 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -774,14 +774,15 @@ export default function CalculatorWireframePreview() {
   </div>
 )}
 </section>
-
+</>
+) : (
+<>
             <section className="grid gap-6 lg:grid-cols-[1fr_360px]">
       {dataBlock}
       {resultBlock}
     </section>
   </>
-        ) : (
-)}
+       )}
 </main>
 </div>
 </div>
